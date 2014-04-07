5030c9c26a478d1b12456cbfbbadc3179262e404
Brian Dignan <briandignan@gmail.com>
2014-04-07 00:23:37 -0400

    Setup base pages and got navbar 'active' highlights to work when navigating between pages.

--- Scala-Play-OneStopWeb/app/controllers/OneStop.scala
+++ Scala-Play-OneStopWeb/app/controllers/OneStop.scala
@@ -14,11 +14,57 @@ import views._
 object OneStop extends Controller with Secured {
 
   /**
-   * Display the dashboard.
+   * Display the home page.
    */
   def index = IsAuthenticated { username => _ =>
     User.findByEmail(username).map { user =>
       Ok(html.index(user))
     }.getOrElse(Forbidden("blah"))
   }
+  
+  /**
+   * Display the home page.
+   */
+  def inventory = IsAuthenticated { username => _ =>
+    User.findByEmail(username).map { user =>
+      Ok(html.inventory(user))
+    }.getOrElse(Forbidden("blah"))
+  }
+  
+  /**
+   * Display the home page.
+   */
+  def customers = IsAuthenticated { username => _ =>
+    User.findByEmail(username).map { user =>
+      Ok(html.customers(user))
+    }.getOrElse(Forbidden("blah"))
+  }
+  
+  /**
+   * Display the home page.
+   */
+  def vendors = IsAuthenticated { username => _ =>
+    User.findByEmail(username).map { user =>
+      Ok(html.vendors(user))
+    }.getOrElse(Forbidden("blah"))
+  }
+  
+  /**
+   * Display the home page.
+   */
+  def reports = IsAuthenticated { username => _ =>
+    User.findByEmail(username).map { user =>
+      Ok(html.reports(user))
+    }.getOrElse(Forbidden("blah"))
+  }
+  
+  /**
+   * Display the home page.
+   */
+  def settings = IsAuthenticated { username => _ =>
+    User.findByEmail(username).map { user =>
+      Ok(html.settings(user))
+    }.getOrElse(Forbidden("blah"))
+  }
+
 }
