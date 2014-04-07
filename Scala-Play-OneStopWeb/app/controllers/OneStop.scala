f25f1262a190e5b414318d381a663ccfc1e6ea0a
Brian Dignan <briandignan@gmail.com>
2014-04-07 09:39:02 -0400

    Factored out common code from each page function into an authenticatedPage function

--- Scala-Play-OneStopWeb/app/controllers/OneStop.scala
+++ Scala-Play-OneStopWeb/app/controllers/OneStop.scala
@@ -5,7 +5,7 @@ import play.api.mvc._
 
 import play.api.data._
 import play.api.data.Forms._
-
+import play.api.templates.HtmlFormat.Appendable
 import anorm._
 
 import models._
@@ -13,67 +13,20 @@ import views._
 
 object OneStop extends Controller with Secured {
 
-  /**
-   * Display the home page.
-   */
-  def index = IsAuthenticated { username => _ =>
-    User.findByEmail(username).map { user =>
-      Ok(html.index(user))
-    }.getOrElse(Forbidden("blah"))
-  }
-  
-  /**
-   * Display the home page.
-   */
-  def inventory = IsAuthenticated { username => _ =>
-    User.findByEmail(username).map { user =>
-      Ok(html.inventory(user))
-    }.getOrElse(Forbidden("blah"))
-  }
-  
-  /**
-   * Display the home page.
-   */
-  def customers = IsAuthenticated { username => _ =>
-    User.findByEmail(username).map { user =>
-      Ok(html.customers(user))
-    }.getOrElse(Forbidden("blah"))
-  }
-  
-  /**
-   * Display the home page.
-   */
-  def vendors = IsAuthenticated { username => _ =>
-    User.findByEmail(username).map { user =>
-      Ok(html.vendors(user))
-    }.getOrElse(Forbidden("blah"))
-  }
-  
-  /**
-   * Display the home page.
-   */
-  def reports = IsAuthenticated { username => _ =>
-    User.findByEmail(username).map { user =>
-      Ok(html.reports(user))
-    }.getOrElse(Forbidden("blah"))
-  }
-  
-  /**
-   * Display the home page.
-   */
-  def settingsUsers = IsAuthenticated { username => _ =>
-    User.findByEmail(username).map { user =>
-      Ok(html.settingsUsers(user))
-    }.getOrElse(Forbidden("blah"))
-  }
-  
-  /**
-   * Display the home page.
-   */
-  def settingsOther = IsAuthenticated { username => _ =>
-    User.findByEmail(username).map { user =>
-      Ok(html.settingsOther(user))
-    }.getOrElse(Forbidden("blah"))
-  }
+	
+  def index = authenticatedPage( html.index(_) )
+  def inventory = authenticatedPage( html.inventory(_) )
+  def customers = authenticatedPage( html.customers(_) )
+  def vendors = authenticatedPage( html.vendors(_) )
+  def reports = authenticatedPage( html.reports(_) )
+  def settingsUsers = authenticatedPage( html.settingsUsers(_) )
+  def settingsOther = authenticatedPage( html.settingsOther(_) )
 
+  
+  private def authenticatedPage( f: User => Appendable): EssentialAction = IsAuthenticated { username => _ =>
+    User.findByEmail(username).map { user =>
+      Ok(f(user))
+    }.getOrElse(Forbidden("blah"))
+  }
+  
 }
