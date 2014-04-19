e7d48cc160bae421b24a605f45b5c0e6f02fa800
Brian Dignan <briandignan@gmail.com>
2014-04-19 14:35:35 -0400

    Applied formatter

--- Scala-Play-OneStopWeb/app/controllers/OneStop.scala
+++ Scala-Play-OneStopWeb/app/controllers/OneStop.scala
@@ -13,21 +13,18 @@ import views._
 
 object OneStop extends Controller with Secured {
 
-	
-  def index = authenticatedPage( html.index(_) )
-  def inventoryList = authenticatedPage( html.inventoryList(_) )
-  def customerList = authenticatedPage( html.customerList(_) )
-  def vendorList = authenticatedPage( html.vendorList(_) )
-  def reports = authenticatedPage( html.reports(_) )
-  def settingsOther = authenticatedPage( html.settingsOther(_) )
+	def index = authenticatedPage( html.index( _ ) )
+	def inventoryList = authenticatedPage( html.inventoryList( _ ) )
+	def customerList = authenticatedPage( html.customerList( _ ) )
+	def vendorList = authenticatedPage( html.vendorList( _ ) )
+	def reports = authenticatedPage( html.reports( _ ) )
+	def settingsOther = authenticatedPage( html.settingsOther( _ ) )
 
-  
-  private def authenticatedPage( f: User => Appendable): EssentialAction = IsAuthenticated { username => _ =>
-    User.findByEmail(username).map { user =>
-      Ok(f(user))
-    }.getOrElse(Forbidden)
-  }
-  
+	private def authenticatedPage( f: User => Appendable ): EssentialAction = IsAuthenticated { username =>
+		_ =>
+			User.findByEmail( username ).map { user =>
+				Ok( f( user ) )
+			}.getOrElse( Forbidden )
+	}
 
-  
 }
