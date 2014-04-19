b268c7921cd1cb4d3578dc687726b557107bd324
Brian Dignan <briandignan@gmail.com>
2014-04-19 14:32:28 -0400

    Reformatted

--- Scala-Play-OneStopWeb/app/controllers/Application.scala
+++ Scala-Play-OneStopWeb/app/controllers/Application.scala
@@ -12,74 +12,68 @@ import views._
 
 object Application extends Controller {
 
-  val loginForm = Form(
-    tuple(
-      "email" -> text,
-      "password" -> text
-      ) verifying ("Invalid email or password", result => result match {
-        case (email, password) => User.authenticate(email, password).isDefined
-    })
-  )
-	
-  /**
-   * Login page.
-   */
-  def login = Action { implicit request => 
-    Ok(html.login(loginForm))
-  }
-  
-  /**
-   * Handle login form submission.
-   */
-  def authenticate = Action { implicit request =>
-    loginForm.bindFromRequest.fold(
-      formWithErrors => BadRequest(html.login(formWithErrors)),
-      user => Redirect(routes.OneStop.index).withSession("email" -> user._1)
-    )
-  }
-  
-  /**
-   * Logout and clean the session.
-   */
-  def logout = Action {
-    Redirect(routes.Application.login).withNewSession.flashing(
-      "success" -> "You've been logged out"
-    )
-  }
+	val loginForm = Form(
+		tuple(
+			"email" -> text,
+			"password" -> text ) verifying ( "Invalid email or password", result => result match {
+				case ( email, password ) => User.authenticate( email, password ).isDefined
+			} ) )
+
+	/**
+	 * Login page.
+	 */
+	def login = Action { implicit request =>
+		Ok( html.login( loginForm ) )
+	}
+
+	/**
+	 * Handle login form submission.
+	 */
+	def authenticate = Action { implicit request =>
+		loginForm.bindFromRequest.fold(
+			formWithErrors => BadRequest( html.login( formWithErrors ) ),
+			user => Redirect( routes.OneStop.index ).withSession( "email" -> user._1 ) )
+	}
+
+	/**
+	 * Logout and clean the session.
+	 */
+	def logout = Action {
+		Redirect( routes.Application.login ).withNewSession.flashing(
+			"success" -> "You've been logged out" )
+	}
 
 }
-
-
 
 /**
  * Provide security features
  */
 trait Secured {
-  
-  /**
-   * Retrieve the connected user email.
-   */
-  private def username(request: RequestHeader) = {
-  	Logger.info("username called")
-  	request.session.get("email")
-  }
-  
-  /**
-   * Redirect to login if the user in not authorized.
-   */
-  private def onUnauthorized(request: RequestHeader) = {
-  	Logger.info("onUnauthorized called")
-  	Results.Redirect(routes.Application.login)
-  }
-  
-  /** 
-   * Action for authenticated users.
-   */
-  def IsAuthenticated(f: => String => Request[AnyContent] => Result) = Security.Authenticated(username, onUnauthorized){ user =>
-  	Action(request => f(user)(request))
-  }
 
-  /*
+	/**
+	 * Retrieve the connected user email.
+	 */
+	private def username( request: RequestHeader ) = {
+		Logger.info( "username called" )
+		request.session.get( "email" )
+	}
+
+	/**
+	 * Redirect to login if the user in not authorized.
+	 */
+	private def onUnauthorized( request: RequestHeader ) = {
+		Logger.info( "onUnauthorized called" )
+		Results.Redirect( routes.Application.login )
+	}
+
+	/**
+	 * Action for authenticated users.
+	 */
+	def IsAuthenticated( f: => String => Request[AnyContent] => Result ) = Security.Authenticated( username, onUnauthorized ) { user =>
+		Action( request => f( user )( request ) )
+	}
+
+	/*
   /**
    * Check if the connected user is a member of this project.
    */
