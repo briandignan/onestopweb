eacdce21cc22b189eceea5fb0f89eb5c3fd9893c
Brian Dignan <briandignan@gmail.com>
2014-04-20 10:56:01 -0400

    Implemented Item list

--- Scala-Play-OneStopWeb/app/models/Item.scala
+++ Scala-Play-OneStopWeb/app/models/Item.scala
@@ -1,25 +1,56 @@
 package models
 
 import anorm._
+import anorm.SqlParser._
+import play.api.db.DB
+import play.api.Play.current
 
 case class Item( 
 		id: Pk[Long] = NotAssigned, 
 		productTypeId: Long, 
 		sku: String, 
 		description: String, 
-		unitPrice: BigDecimal, 
+		unitPrice: java.math.BigDecimal, 
 		quantityOnHand: Int, 
 		quantityPerOrder: Int, 
 		quantityLowPoint: Int )
 
 object Item {
 
-	def findById(id: Long): Option[Item] = {
+	
+	
+	val simpleParser = 
+		get[Pk[Long]]( "Inventory.ItemID" ) ~
+		get[Long]( "Inventory.ProductTypeID" ) ~
+		get[String]( "Inventory.SKU" ) ~
+		get[String]( "Inventory.Description" ) ~
+		get[java.math.BigDecimal]( "Inventory.UnitPrice" ) ~
+		get[Int]( "Inventory.QuantityOnHand" ) ~
+		get[Int]( "Inventory.QuantityPerOrder" ) ~
+		get[Int]( "Inventory.QuantityLowPoint" ) map {
+			case id ~ productTypeId ~ sku ~ description ~ unitPrice ~ quantityOnHand ~ quantityPerOrder ~ quantityLowPoint => Item( id, productTypeId, sku, description, unitPrice, quantityOnHand, quantityPerOrder, quantityLowPoint ) 
+		}
+	
+	val withProductType = Item.simpleParser ~ ProductType.simpleParser map {
+		case item ~ productType => ( item, productType )
+	}
+		
+		
+		
+	
+	def findById(id: Long): Option[(Item, ProductType)] = {
 		None
 	}
 	
-	def findAll(): Seq[Item] = {
-		Seq()
+	def findAll(): Seq[(Item, ProductType)] = {
+		DB.withConnection { implicit connection => 
+			SQL(
+				"""
+					SELECT * FROM Inventory 
+					JOIN ProductType ON Inventory.ProductTypeID = ProductType.ProductTypeID
+				"""
+			).as(Item.withProductType *)
+		}
 	}
 	
 	def create( item: Item ): Int = {
