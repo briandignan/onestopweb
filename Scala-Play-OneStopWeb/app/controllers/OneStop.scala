61c8daf1e168a075d64e7436f5facaabcb464f44
Brian Dignan <briandignan@gmail.com>
2014-04-20 10:56:25 -0400

    Moved Item to its own controller

--- Scala-Play-OneStopWeb/app/controllers/OneStop.scala
+++ Scala-Play-OneStopWeb/app/controllers/OneStop.scala
@@ -14,7 +14,6 @@ import views._
 object OneStop extends Controller with Secured {
 
 	def index = authenticatedPage( html.index( _ ) )
-	def itemList = authenticatedPage( html.itemList( _ ) )
 	def customerList = authenticatedPage( html.customerList( _ ) )
 	def vendorList = authenticatedPage( html.vendorList( _ ) )
 	def reports = authenticatedPage( html.reports( _ ) )
