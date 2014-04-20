c53b1be6a2561c1e3a0466ec2f0d863d8d1f7cdd
Brian Dignan <briandignan@gmail.com>
2014-04-19 20:36:50 -0400

    Added basic template for the items and departments views

--- Scala-Play-OneStopWeb/app/controllers/OneStop.scala
+++ Scala-Play-OneStopWeb/app/controllers/OneStop.scala
@@ -14,7 +14,8 @@ import views._
 object OneStop extends Controller with Secured {
 
 	def index = authenticatedPage( html.index( _ ) )
-	def inventoryList = authenticatedPage( html.inventoryList( _ ) )
+	def itemList = authenticatedPage( html.itemList( _ ) )
+	def departmentList = authenticatedPage( html.departmentList( _ ) )
 	def customerList = authenticatedPage( html.customerList( _ ) )
 	def vendorList = authenticatedPage( html.vendorList( _ ) )
 	def reports = authenticatedPage( html.reports( _ ) )
