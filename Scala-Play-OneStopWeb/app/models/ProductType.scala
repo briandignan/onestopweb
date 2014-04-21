a4ae22f5ea537931ad8ccd1dffb61671f7936abf
Brian Dignan <briandignan@gmail.com>
2014-04-20 23:59:01 -0400

    Updated formatting

--- Scala-Play-OneStopWeb/app/models/ProductType.scala
+++ Scala-Play-OneStopWeb/app/models/ProductType.scala
@@ -9,50 +9,50 @@ import anorm.SqlParser._
 case class ProductType( id: Pk[Long] = NotAssigned, name: String )
 
 object ProductType {
-	
+
 	val simpleParser = {
 		get[Pk[Long]]( "ProductType.ProductTypeID" ) ~
-		get[String]( "ProductType.Name" ) map {
-			case id ~ name => ProductType( id, name )
-		}
+			get[String]( "ProductType.Name" ) map {
+				case id ~ name => ProductType( id, name )
+			}
 	}
-	
+
 	def findById( id: Long ): Option[ProductType] = {
-		DB.withConnection { implicit connection => 
+		DB.withConnection { implicit connection =>
 			SQL(
 				"""
 					SELECT * FROM ProductType 
 					WHERE ProductTypeID = {id}
-				"""
+				""" 
 			).on(
-				'id -> id
-			).as(ProductType.simpleParser.singleOpt)
+				'id -> id 
+			).as( ProductType.simpleParser.singleOpt )
 		}
 	}
-	
+
 	def findByName( name: String ): Option[ProductType] = {
-		DB.withConnection { implicit connection => 
+		DB.withConnection { implicit connection =>
 			SQL(
 				"""
 					SELECT * FROM ProductType 
 					WHERE Name = {name}
-				"""
+				""" 
 			).on(
-				'name -> name
-			).as(ProductType.simpleParser.singleOpt)
+				'name -> name 
+			).as( ProductType.simpleParser.singleOpt )
 		}
 	}
-	
+
 	def findAll(): Seq[ProductType] = {
 		DB.withConnection { implicit connection =>
 			SQL(
 				"""
 					SELECT * from ProductType
-				"""
-			).as(ProductType.simpleParser *)
+				""" 
+			).as( ProductType.simpleParser * )
 		}
 	}
-	
+
 	def create( productType: ProductType ): Int = {
 		DB.withConnection { implicit connection =>
 			SQL(
@@ -60,39 +60,50 @@ object ProductType {
 					INSERT INTO ProductType 
 					(Name) VALUES 
 					({name})
-				"""
+				""" 
 			).on(
-				'name -> productType.name
+				'name -> productType.name 
 			).executeUpdate()
 		}
 	}
-	
+
 	def update( id: Long, productType: ProductType ): Int = {
-		DB.withConnection { implicit connection => 
+		DB.withConnection { implicit connection =>
 			SQL(
 				"""
 					UPDATE ProductType
 					SET Name = {name} 
 					WHERE ProductTypeID = {id}
-				"""
+				""" 
 			).on(
 				'id -> id,
-				'name -> productType.name
+				'name -> productType.name 
 			).executeUpdate()
 		}
 	}
-	
+
 	def delete( id: Long ): Int = {
-		DB.withConnection { implicit connection => 
+		DB.withConnection { implicit connection =>
 			SQL(
 				"""
 					DELETE FROM ProductType 
 					WHERE ProductTypeID = {id}
-				"""
+				""" 
 			).on(
-				'id -> id
+				'id -> id 
 			).executeUpdate()
 		}
 	}
-	
+
+	/**
+	 * Construct the (id,productType) mapping needed to fill a select options set.
+	 */
+	def options: Seq[( String, String )] = DB.withConnection { implicit connection =>
+		SQL( 
+			"""
+				SELECT * FROM ProductType ORDER BY Name
+			""" 
+		).as( ProductType.simpleParser * ).map( pt => ( pt.id.toString , pt.name ) )
+	}
+
 }
