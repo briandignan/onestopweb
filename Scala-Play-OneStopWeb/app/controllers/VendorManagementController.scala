bf179b908166711ee0a0a0b27ba6145517e97219
Brian Dignan <briandignan@gmail.com>
2014-04-21 16:48:52 -0400

    Base template

---
+++ Scala-Play-OneStopWeb/app/controllers/VendorManagementController.scala
@@ -0,0 +1,29 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+import play.api.data._
+import play.api.data.validation.Constraint
+import play.api.data.validation.ValidationResult
+import play.api.data.validation.Valid
+import play.api.data.validation.Invalid
+import play.api.data.Forms._
+import play.api.templates.HtmlFormat.Appendable
+
+import anorm._
+
+import models._
+import views._
+
+object VendorManagementController extends Controller with Secured {
+
+	def list() = IsAuthenticated { username => 
+		implicit request => {
+			User.findByEmail(username).map { user =>
+				Ok( html.vendorManagementList( user ) )
+			}.getOrElse( Forbidden )
+		}
+	}
+	
+}
